23784c055e60c30c3b20a2b534ce9d0bdb7e3fb2
Alan <Alan_manuel_gonzalez@hotmail.com>
2024-02-28 11:36:48 -0300

    fix calcular distancia

--- src/components/calculateDistance/CalculateDistance.jsx
+++ src/components/calculateDistance/CalculateDistance.jsx
@@ -7,11 +7,44 @@ function CalculateDistance() {
   const [distance, setDistance] = useState(null);
   const [popupVisible, setPopupVisible] = useState(false);
 
-  // Coordenadas geográficas predefinidas
-  const originCoordinates = { lat: -34.6269269, lon: -58.6431527 }; // Coordenadas de un lugar en Buenos Aires
-  const destinationCoordinates = { lat: -34.6154321, lon: -58.4598423 }; // Coordenadas de otro lugar en Buenos Aires
+  const handleCalculateDistance = async () => {
+    if (!origin || !destination) {
+      alert('Por favor ingrese tanto el origen como el destino');
+      return;
+    }
 
-  // Función para calcular la distancia entre dos puntos geográficos utilizando la fórmula de Haversine
+    try {
+      const [originCoordinates, destinationCoordinates] = await Promise.all([
+        getCoordinates(origin),
+        getCoordinates(destination)
+      ]);
+
+      const calculatedDistance = calculateDistance(
+        originCoordinates.lat,
+        originCoordinates.lon,
+        destinationCoordinates.lat,
+        destinationCoordinates.lon
+      );
+
+      setDistance(`${calculatedDistance} km`);
+    } catch (error) {
+      console.error('Error al obtener las coordenadas:', error);
+      alert('Error al obtener las coordenadas. Por favor, inténtelo de nuevo.');
+    }
+  };
+
+  const getCoordinates = async (address) => {
+    const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=AIzaSyDMFGskIfEG9Xy3hNa0hcTtgL3h-ffdS9E`);
+    const data = await response.json();
+
+    if (data.results && data.results.length > 0) {
+      const { lat, lng } = data.results[0].geometry.location;
+      return { lat, lon: lng };
+    } else {
+      throw new Error('No se encontraron coordenadas para la dirección proporcionada');
+    }
+  };
+
   const calculateDistance = (lat1, lon1, lat2, lon2) => {
     const R = 6371; // Radio de la Tierra en kilómetros
     const dLat = (lat2 - lat1) * (Math.PI / 180); // Diferencia de latitud en radianes
@@ -20,25 +53,8 @@ function CalculateDistance() {
       Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.cos((lat1 * Math.PI) / 180) * Math.cos((lat2 * Math.PI) / 180) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c; // Distancia en kilómetros
-    return distance.toFixed(2); // Redondeamos la distancia a dos decimales
-  };
-
-  const handleCalculateDistance = () => {
-    if (!origin || !destination) {
-      alert('Por favor ingrese tanto el origen como el destino');
-      return;
-    }
-
-    // Calcular la distancia utilizando la fórmula de Haversine
-    const calculatedDistance = calculateDistance(
-      originCoordinates.lat,
-      originCoordinates.lon,
-      destinationCoordinates.lat,
-      destinationCoordinates.lon
-    );
-
-    setDistance(`${calculatedDistance} km`);
+    const distance = R * c;
+    return distance.toFixed(2);
   };
 
   const togglePopup = () => {
@@ -51,7 +67,7 @@ function CalculateDistance() {
         <div className={styles.popup}>
           <div className={styles.popupContent}>
             <h2>Calcular Distancia</h2>
-            <label>Origen 
+            <label>Origen:
             <input
               type="text"
               value={origin}
@@ -61,7 +77,7 @@ function CalculateDistance() {
             </label>
             
 
-            <label>Destino 
+            <label>Destino:
             <input
               type="text"
               value={destination}
@@ -73,6 +89,9 @@ function CalculateDistance() {
             <button onClick={handleCalculateDistance} className={styles.calculateButton}>Calcular</button>
             <button onClick={togglePopup} className={styles.calculateButton}>Cerrar</button>
             {distance && <p>La distancia es: {distance}</p>}
+            {distance && <p>El precio por 1 viaje es: ${(parseFloat(distance) * 1000).toFixed(2)}</p>}
+
+
           </div>
         </div>
       )}
